65d3b0157ad2f5725067fd0f647c01df38aeaa01
Mathanstunnerz <mathanstunnerz95@gmail.com>
2023-02-23 14:32:32 +0530

    add hours minutes secounds

--- date-time.js
+++ date-time.js
@@ -1,15 +1,21 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const date = Date.now();
-const path = Math.floor(date / 1000.0);
-
-
+const Timestamp = Date.now();
+const path = Math.floor(Timestamp  / 1000.0);
+const date = new Date()
+const hours  = date.getHours();
+const minutes = date.getMinutes();
+const second = date.getSeconds();
+// console.log(second)
+// console.log(minutes)
+// console.log(hours)
 // http://localhost:5787;
 import express from "express";
 const app = express();
 const PORT = process.env.PORT;
 app.get("/", function (request, response) {
-  response.send(`current time stamp ${path} `);
+  response.send(`current time stamp ${path} , hours ${hours} : minutes ${minutes} : second ${second}`)
 });
 app.listen(PORT, () => console.log("SERVER CONNECTED"));
+
